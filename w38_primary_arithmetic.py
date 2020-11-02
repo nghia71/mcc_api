30fcd62551c6fbaa2b4a4a954380299e68db7f8f
Nghia Doan <nghia71@gmail.com>
2020-11-01 20:09:28 -0500

    Solutions added.

--- w38_primary_arithmetic.py
+++ w38_primary_arithmetic.py
@@ -5,8 +5,21 @@ from mcc_api import Pegasus
 # Demonstrate to find the number of carry operation and the sum
 #
 def primary_arithmetic(i_j):
-    print(i_j)
-    return '3 1010'
+    i, j = [''.join(reversed(e)) for e in i_j.split()]
+    c, cc, s = 0, 0, []
+    for k in range(0, max([len(i), len(j)])):
+        ik = int(i[k]) if k < len(i) else 0
+        jk = int(j[k]) if k < len(j) else 0
+        sk = ik + jk + c
+        c = 0
+        if sk > 9:
+            sk = sk - 10
+            c = 1
+            cc += 1
+        s.append(str(sk))
+    if c == 1:
+        s.append('1')
+    return '%s %s' % (''.join(reversed(s)), cc)
 
 
 if __name__ == '__main__':
@@ -16,10 +29,12 @@ if __name__ == '__main__':
     ########################################
     # Receive the two numbers via Pegasus.
     i_and_j = p.attempt('primary_arithmetic')
+    print('i_and_j', i_and_j)
 
     ########################################
     # Figure out the number of carry operations and the sum and submit to Astria
     carry_sum = primary_arithmetic(i_and_j)
+    print('carry_sum', carry_sum)
     result = p.submit('primary_arithmetic', i_and_j, carry_sum)
 
     ########################################
--- w38_where_waldorf.py
+++ w38_where_waldorf.py
@@ -5,8 +5,22 @@ from mcc_api import Pegasus
 # Demonstrate to find the number of carry operations and the sum
 #
 def where_waldorf(word_m_n_grid):
-    print(word_m_n_grid)
-    return '1 6'
+    splits = word_and_m_n_grid.lower().split('\n')
+    word, grid = splits[0], [[e for e in row] for row in splits[2:]]
+    word_len = len(word)
+    m, n = [int(e) for e in splits[1].split()]
+    for m_pos in range(0, m-1):
+        for n_pos in range(0, n-1):
+            count = 1
+            for direction in [[0, 1], [1, 1], [1, 0]]:
+                for k in range(1, len(word)):
+                    i, j = m_pos + k*direction[0], n_pos + k*direction[1]
+                    if i >= m or j >= n or grid[i][j] != word[k]:
+                        break
+                    count += 1
+                if count == word_len:
+                    return '%s %s' % (m_pos, n_pos)
+    return ''
 
 
 if __name__ == '__main__':
@@ -16,10 +30,12 @@ if __name__ == '__main__':
     ########################################
     # Receive word, dimensions, and the grid via Pegasus
     word_and_m_n_grid = p.attempt('where_waldorf')
+    print(word_and_m_n_grid)
 
     ########################################
     # Figure out where the word is and submit to Astria
     coordinates = where_waldorf(word_and_m_n_grid)
+    print(coordinates)
     result = p.submit('where_waldorf', word_and_m_n_grid, coordinates)
 
     ########################################
